2dc2f143dc2c02d21d2fe115f734af4483fdcfc6
Danny Ngo <70412140+steve8D@users.noreply.github.com>
2022-01-15 22:29:23 -0800

    Merging Home Page into the website (good luck Jamie)

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,8 +1,12 @@
 import Header from './components/header/Header'
+import HomePage from './components/homepage/HomePage'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <HomePage />
+    </>
   );
 }
 
