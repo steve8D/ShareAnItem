77ee03fff941b50088257b521715391678b9ba65
Void <47502826+aidavoid@users.noreply.github.com>
2022-01-16 15:55:47 +0700

    Update SignUp.js

--- hackathon/src/SignUp.js
+++ hackathon/src/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card} from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
+import { useAuth } from "../context/AuthContext"
 import { Link} from "react-router-dom"
 
 export default function Signup(){
