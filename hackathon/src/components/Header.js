6dc065f4b2c1c0ab46e0b972b25a931b59a6a653
Danny Ngo <70412140+steve8D@users.noreply.github.com>
2022-01-15 21:10:35 -0800

    Cleanup pt.2

--- hackathon/src/components/Header.js
+++ hackathon/src/components/Header.js
@@ -1,40 +1,23 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col } from "react-bootstrap";
-import HeaderAppLogo from "./header/HeaderAppLogo";
-import Home from '../assets/Icons/Home.svg';
 import '../css/header.css';
 
-/**
- * Top header for functional icons and search bar.
- */
 class Header extends Component {
     render() {
         return (
             <div className="header">
                 <Row xs="auto">
                     <Col>
-                        <FunctionalIcon icon={Home} path="/"/>
+                        <a className="app-name" href="/">Home</a>
                     </Col>
                     <Col>
-                        <HeaderAppLogo />
+                        <a className="app-name" href="/">My Orders</a>
                     </Col>
                 </Row>
             </div>
         );
     }
-}
-
-class FunctionalIcon extends Component {
-    render() {
-        let iconSrc = this.props.icon;
-        let path = this.props.path;
-        return (
-            <a href={path | "#"}>
-                <img src={iconSrc}></img>
-            </a>
-        );
-    };
 }
 
 export default Header;
--- hackathon/src/components/header/HeaderAppLogo.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class HeaderAppLogo extends Component {
-    render() {
-        return <a className="app-name" href="/">My Orders</a>;
-    }
-}
-
-export default HeaderAppLogo;
