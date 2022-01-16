6b8a3b663697c375581d3f2f36afaac291220423
Void <47502826+aidavoid@users.noreply.github.com>
2022-01-16 15:40:52 +0700

    Update App.js

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,12 +1,30 @@
 import Header from './components/header/Header'
 import HomePage from './components/homepage/HomePage'
+import React from "react";
+import Signup from "./Signup";
+import Login from "./Login"
+import {Container} from "react-bootstrap"
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <>
-      <Header />
-      <HomePage />
-    </>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
